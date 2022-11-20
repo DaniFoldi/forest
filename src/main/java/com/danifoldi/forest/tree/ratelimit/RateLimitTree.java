02eb36a4fb3e52f138c9d787de203462595c628b
Dániel Földi <dani@stefan.hu>
2022-11-20 17:28:07 +0000

    :zap: Add ratelimit tree

---
+++ src/main/java/com/danifoldi/forest/tree/ratelimit/RateLimitTree.java
@@ -0,0 +1,37 @@
+package com.danifoldi.forest.tree.ratelimit;
+
+import com.danifoldi.forest.seed.Tree;
+import com.danifoldi.microbase.Microbase;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
+@SuppressWarnings("unused")
+public class RateLimitTree implements Tree {
+
+    private static final Map<String, Instant> ratelimits = new ConcurrentHashMap<>();
+
+    @Override
+    public @NotNull CompletableFuture<?> load() {
+        return CompletableFuture.runAsync(ratelimits::clear, Microbase.getThreadPool());
+    }
+
+    @Override
+    public @NotNull CompletableFuture<@NotNull Boolean> unload(boolean force) {
+        return CompletableFuture.supplyAsync(() -> {
+            ratelimits.clear();
+            return true;
+        }, Microbase.getThreadPool());
+    }
+
+    public static boolean rateLimit(String key, Integer length, ChronoUnit unit) {
+        Instant old = ratelimits.getOrDefault(key, Instant.MIN);
+        Instant now = Instant.now();
+        ratelimits.put(key, now.plus(length, unit));
+        return old.isBefore(now);
+    }
+}
