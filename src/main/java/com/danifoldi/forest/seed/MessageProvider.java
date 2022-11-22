e6547a144f3b44edc1f9ffc06ebf93be0c1ef6a2
Dániel Földi <dani@stefan.hu>
2022-11-22 10:46:57 +0000

    :recycle: Update messageprovider

--- src/main/java/com/danifoldi/forest/seed/MessageProvider.java
+++ src/main/java/com/danifoldi/forest/seed/MessageProvider.java
@@ -5,13 +5,14 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class MessageProvider {
 
-    private static Map<String, String> messageCache = new ConcurrentHashMap<>();
+    private static final Map<String, String> messageCache = new ConcurrentHashMap<>();
 
     public static String provide(String key) {
         return messageCache.getOrDefault(key, "?");
     }
 
     public static void update(Map<String, String> values) {
+        messageCache.clear();
         messageCache.putAll(values);
     }
 }
