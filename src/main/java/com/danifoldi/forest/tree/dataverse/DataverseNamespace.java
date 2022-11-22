42326321352593712146bed6446e6a38d352cacd
Dániel Földi <dani@stefan.hu>
2022-11-22 10:47:20 +0000

    :zap: Add static namespace instance

--- src/main/java/com/danifoldi/forest/tree/dataverse/DataverseNamespace.java
+++ src/main/java/com/danifoldi/forest/tree/dataverse/DataverseNamespace.java
@@ -4,6 +4,13 @@ import com.danifoldi.dataverse.data.Namespaced;
 import org.jetbrains.annotations.NotNull;
 
 public class DataverseNamespace implements Namespaced {
+
+    public static DataverseNamespace get() {
+        return instance;
+    }
+
+    private static DataverseNamespace instance = new DataverseNamespace();
+
     @Override
     public @NotNull String getNamespace() {
         return "Forest";
