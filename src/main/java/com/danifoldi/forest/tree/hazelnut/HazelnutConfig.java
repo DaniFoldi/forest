b1156ae694c0de5f443107f62100e05e96d061b4
Dániel Földi <dani@stefan.hu>
2022-11-24 12:27:45 +0000

    :hammer: Add defaults to hazelnut config

--- src/main/java/com/danifoldi/forest/tree/hazelnut/HazelnutConfig.java
+++ src/main/java/com/danifoldi/forest/tree/hazelnut/HazelnutConfig.java
@@ -1,10 +1,10 @@
 package com.danifoldi.forest.tree.hazelnut;
 
 public class HazelnutConfig {
-    public long cacheExpiryRate;
-    public long cacheHousekeeperRate;
-    public long heartbeatRate;
-    public String redisHost;
-    public String redisUsername;
-    public String redisPassword;
+    public long cacheExpiryRate = hazelnut.core.config.HazelnutConfig.DEFAULT_CACHE_EXPIRY_RATE;
+    public long cacheHousekeeperRate = hazelnut.core.config.HazelnutConfig.DEFAULT_CACHE_HOUSEKEEPER_RATE;
+    public long heartbeatRate = hazelnut.core.config.HazelnutConfig.DEFAULT_HEARTBEAT_RATE;
+    public String redisHost = "";
+    public String redisUsername = "";
+    public String redisPassword = "";
 }
