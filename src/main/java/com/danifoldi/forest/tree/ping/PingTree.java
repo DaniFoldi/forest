147fb7537bccde060ec3ab547e43918d1ea32b23
Dániel Földi <dani@stefan.hu>
2022-11-28 10:35:32 +0000

    :recycle: Change pong response to console

--- src/main/java/com/danifoldi/forest/tree/ping/PingTree.java
+++ src/main/java/com/danifoldi/forest/tree/ping/PingTree.java
@@ -3,7 +3,10 @@ package com.danifoldi.forest.tree.ping;
 import com.danifoldi.forest.seed.GrownTrees;
 import com.danifoldi.forest.seed.Tree;
 import com.danifoldi.forest.seed.collector.collector.CommandCollector;
+import com.danifoldi.forest.seed.collector.collector.DependencyCollector;
+import com.danifoldi.forest.seed.collector.collector.MessageCollector;
 import com.danifoldi.forest.seed.collector.collector.PermissionCollector;
+import com.danifoldi.forest.seed.collector.collector.VersionCollector;
 import com.danifoldi.forest.tree.command.CommandTree;
 import com.danifoldi.microbase.BaseMessage;
 import com.danifoldi.microbase.BasePlayer;
@@ -16,6 +19,9 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.CompletableFuture;
 
+@VersionCollector("1.0.0")
+@DependencyCollector(tree="command", minVersion="1.0.0")
+@DependencyCollector(tree="message", minVersion="1.0.0")
 public class PingTree implements Tree, CommandContainer {
     @Override
     public @NotNull CompletableFuture<?> load() {
@@ -32,13 +38,15 @@ public class PingTree implements Tree, CommandContainer {
 
     @CommandCollector("ping")
     @PermissionCollector("forest.ping.command.ping")
+    @MessageCollector(value = "command.ping.pong", replacements = {"{ping}"})
+    @MessageCollector("command.ping.consolepong")
     @CommandDefinition(route = "ping", permission = "forest.ping.command.ping", runAsync = true)
     public void pingCommand(@Source BaseSender sender) {
-        BaseMessage message = Microbase.baseMessage().providedText("command.ping.pong");
+        BaseMessage message;
         if (sender instanceof BasePlayer player) {
-            message = message.replace("{ping}", String.valueOf(player.ping()));
+            message = Microbase.baseMessage().providedText("command.ping.pong").replace("{ping}", String.valueOf(player.ping()));
         } else {
-            message = message.replace("{ping}", Microbase.provideMessage("command.ping.consoleping"));
+            message = Microbase.baseMessage().providedText("command.ping.consolepong");
         }
         sender.send(message);
     }
