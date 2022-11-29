dadea327cddf82641eb7b89ab3538372b3de5b05
Dániel Földi <dani@stefan.hu>
2022-11-29 22:56:18 +0000

    :zap: load more metadata about trees

--- src/main/java/com/danifoldi/forest/seed/TreeInfo.java
+++ src/main/java/com/danifoldi/forest/seed/TreeInfo.java
@@ -3,6 +3,8 @@ package com.danifoldi.forest.seed;
 import com.danifoldi.dml.DmlParser;
 import com.danifoldi.dml.exception.DmlParseException;
 import com.danifoldi.dml.type.DmlObject;
+import com.danifoldi.dml.type.DmlString;
+import com.danifoldi.dml.type.DmlValue;
 import com.danifoldi.microbase.Microbase;
 
 import java.io.BufferedReader;
@@ -11,6 +13,8 @@ import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.logging.Level;
@@ -18,7 +22,10 @@ import java.util.stream.Collectors;
 
 public class TreeInfo {
     public String version;
-    public Set<String> dependencies;
+    public Map<String, String> dependencies;
+    public Set<String> platforms;
+    public Set<String> commands;
+    public Map<String, List<String>> messages;
     public final ClassLoader classLoader;
     public final String className;
     public final String pack;
@@ -41,9 +48,12 @@ public class TreeInfo {
                 String dmlString = reader.lines().collect(Collectors.joining("\n"));
                 DmlObject dml = DmlParser.parse(dmlString).asObject();
                 version = dml.get("version").toString();
-                dependencies = dml.get("dependencies").asArray().value().stream().map(v -> v.asString().value()).collect(Collectors.toSet());
+                commands = dml.get("commands").asArray().value().stream().map(DmlValue::asString).map(DmlString::value).collect(Collectors.toSet());
+                platforms = dml.get("platforms").asArray().value().stream().map(DmlValue::asString).map(DmlString::value).collect(Collectors.toSet());
+                messages = dml.get("messages").asArray().value().stream().map(DmlValue::asObject).collect(Collectors.toMap(v -> v.get("template").asString().value(), v -> v.get("replacements").asArray().value().stream().map(r -> r.asString().value()).toList()));
+                dependencies = dml.get("dependencies").asArray().value().stream().map(DmlValue::asObject).collect(Collectors.toMap(v -> v.get("tree").asString().value(), v -> v.get("minVersion").asString().value()));
             } catch (DmlParseException | IOException e) {
-                Microbase.logger.log(Level.SEVERE, "Could not load tree metadata %s".formatted(pack));
+                Microbase.logger.log(Level.SEVERE, "Could not load tree metadata %s: %s".formatted(pack, e.getMessage()));
                 return false;
             }
             tree = treeClass.getConstructor().newInstance();
