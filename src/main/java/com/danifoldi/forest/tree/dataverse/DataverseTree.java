e63db0f4a20aca2849815dc39c5a2daca400cebf
Dániel Földi <dani@stefan.hu>
2022-11-29 11:40:07 +0000

    :zap: Add pattern, instant and throwable as dataverse types

--- src/main/java/com/danifoldi/forest/tree/dataverse/DataverseTree.java
+++ src/main/java/com/danifoldi/forest/tree/dataverse/DataverseTree.java
@@ -2,20 +2,39 @@ package com.danifoldi.forest.tree.dataverse;
 
 import com.danifoldi.dataverse.DataVerse;
 import com.danifoldi.forest.seed.Tree;
-import com.danifoldi.forest.seed.collector.collector.DependencyCollector;
 import com.danifoldi.forest.seed.collector.collector.VersionCollector;
 import com.danifoldi.microbase.Microbase;
 import org.jetbrains.annotations.NotNull;
 
+import java.time.Instant;
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
+import java.util.regex.Pattern;
 
 @VersionCollector("1.0.0")
 public class DataverseTree implements Tree {
 
     @Override
     public @NotNull CompletableFuture<?> load() {
-        //noinspection ResultOfMethodCallIgnored
-        return CompletableFuture.runAsync(DataVerse::getDataVerse, Microbase.getThreadPool("dataverse"));
+        return CompletableFuture.runAsync(() -> {
+            DataVerse.getDataVerse().getTranslationEngine().addJavaTypeToMysqlColumn("java.util.regex.Pattern", "VARCHAR(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+            DataVerse.getDataVerse().getTranslationEngine().addJavaTypeToMysqlQuery("java.util.regex.Pattern", (statement, i, spec, obj) -> statement.setString(i, ((Pattern)spec.reflect().get(obj)).pattern()));
+            DataVerse.getDataVerse().getTranslationEngine().addMysqlResultToJavaType("java.util.regex.Pattern", (results, colName, spec, obj) -> spec.reflect().set(obj, Pattern.compile(results.getString(colName))));
+
+            DataVerse.getDataVerse().getTranslationEngine().addJavaTypeToMysqlColumn("java.time.Instant", "VARCHAR(2048) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+            DataVerse.getDataVerse().getTranslationEngine().addJavaTypeToMysqlQuery("java.time.Instant", (statement, i, spec, obj) -> statement.setString(i, ((Instant)spec.reflect().get(obj)).toString()));
+            DataVerse.getDataVerse().getTranslationEngine().addMysqlResultToJavaType("java.time.Instant", (results, colName, spec, obj) -> spec.reflect().set(obj, Instant.parse(results.getString(colName))));
+
+            DataVerse.getDataVerse().getTranslationEngine().addJavaTypeToMysqlColumn("java.lang.Throwable", "MEDIUMTEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+            DataVerse.getDataVerse().getTranslationEngine().addJavaTypeToMysqlQuery("java.lang.Throwable", (statement, i, spec, obj) -> {
+                Throwable throwable = (Throwable)spec.reflect().get(obj);
+                String details = "%s____%s".formatted(throwable.getMessage(), Arrays.stream(throwable.getStackTrace()).map(StackTraceElement::toString));
+                statement.setString(i, details);
+            });
+
+            // NOTE stack trace is not rebuilt
+            DataVerse.getDataVerse().getTranslationEngine().addMysqlResultToJavaType("java.lang.Throwable", (results, colName, spec, obj) -> spec.reflect().set(obj, new Throwable(results.getString(colName))));
+        }, Microbase.getThreadPool("dataverse"));
     }
 
     @Override
