b084da03281f9f502a358db01f6d1e1c782e51d9
Dániel Földi <dani@stefan.hu>
2022-12-04 22:40:54 +0000

    :zap: Add messages to command tree

--- src/main/java/com/danifoldi/forest/tree/command/CommandTree.java
+++ src/main/java/com/danifoldi/forest/tree/command/CommandTree.java
@@ -5,6 +5,7 @@ import com.danifoldi.dataverse.data.NamespacedMultiDataVerse;
 import com.danifoldi.forest.seed.MessageProvider;
 import com.danifoldi.forest.seed.Tree;
 import com.danifoldi.forest.seed.collector.collector.DependencyCollector;
+import com.danifoldi.forest.seed.collector.collector.MessageCollector;
 import com.danifoldi.forest.seed.collector.collector.VersionCollector;
 import com.danifoldi.forest.tree.dataverse.DataverseNamespace;
 import com.danifoldi.microbase.BaseSender;
@@ -25,6 +26,23 @@ public class CommandTree implements Tree {
     CommandDispatcher<BaseSender> dispatcher;
     NamespacedMultiDataVerse<CommandConfig> commandDataverse;
 
+    @MessageCollector(value="forest.command.condition.failed", replacements={"{id}"})
+    @MessageCollector(value="forest.command.dispatcher.authorization-error", replacements={"{permission}"})
+    @MessageCollector(value="forest.command.dispatcher.failed-to-execute-command", replacements={"{commandline}"})
+    @MessageCollector(value="forest.command.dispatcher.no-such-command", replacements={"{name}"})
+    @MessageCollector(value="forest.command.dispatcher.too-few-arguments", replacements={"{syntax}"})
+    @MessageCollector(value="forest.command.dispatcher.too-many-arguments", replacements={"{syntax}"})
+    @MessageCollector(value="forest.command.dispatcher.illegal-command-source", replacements={"{found}", "{required}"})
+    @MessageCollector(value="forest.command.parameter.invalid-boolean-value", replacements={"{input}", "{options}"})
+    @MessageCollector(value="forest.command.parameter.invalid-character-value", replacements={"{input}"})
+    @MessageCollector(value="forest.command.parameter.invalid-number-value", replacements={"{input}"})
+    @MessageCollector(value="forest.command.parameter.number-out-of-range", replacements={"{min}", "{max}"})
+    @MessageCollector(value="forest.command.parameter.quoted-string-invalid-trailing-character", replacements={"{input}"})
+    @MessageCollector(value="forest.command.parameter.string-regex-error", replacements={"{input}", "{regex}"})
+    @MessageCollector(value="forest.command.parameter.missing-flag-value", replacements={"{input}"})
+    @MessageCollector(value="forest.command.parameter.missing-flag", replacements={"{syntax}"})
+    @MessageCollector(value="forest.command.parameter.duplicate-flag", replacements={"{flag}"})
+    @MessageCollector(value="forest.command.parameter.unrecognized-command-flag", replacements={"{input}"})
     @Override
     public @NotNull CompletableFuture<?> load() {
         return CompletableFuture.runAsync(() -> {
@@ -33,7 +51,7 @@ public class CommandTree implements Tree {
             dispatcher = CommandDispatcher.builder(TypeToken.of(BaseSender.class))
                     .withAuthorizer(BaseSender::hasPermission)
                     .withMessenger(BaseSender::send)
-                    .withMessageProvider(key -> MessageProvider.provide(key.key()))
+                    .withMessageProvider(key -> MessageProvider.provide("forest.command.%s".formatted(key.key())))
                     .withAsyncExecutor(Microbase.getThreadPool("command"))
                     .withRegistrationHandler(new CommandRegistrationHandler(commandDataverse))
                     .build();
