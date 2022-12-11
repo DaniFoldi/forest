c2d30f2759af09d0042038530b308e66f7de76e6
Dániel Földi <dani@stefan.hu>
2022-12-11 22:58:51 +0100

    :zap: warn if a message is missing

--- src/main/java/com/danifoldi/forest/seed/MessageProvider.java
+++ src/main/java/com/danifoldi/forest/seed/MessageProvider.java
@@ -1,14 +1,21 @@
 package com.danifoldi.forest.seed;
 
+import com.danifoldi.microbase.Microbase;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Level;
 
 public class MessageProvider {
 
     private static final Map<String, String> messageCache = new ConcurrentHashMap<>();
 
     public static String provide(String key) {
-        return messageCache.getOrDefault(key, "?");
+        if (!messageCache.containsKey(key)) {
+            messageCache.put(key, key);
+            Microbase.logger.log(Level.WARNING, "Key %s is missing from providable messages".formatted(key));
+        }
+        return messageCache.get(key);
     }
 
     public static void update(Map<String, String> values) {
